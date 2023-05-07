1bfbdd3c921ae201194ccfdff7449e4543e7b4a2
Claudio Vasquez <claudio.vasquez@unah.hn>
2023-05-07 12:59:27 -0600

    IOT simulator

--- testSenderMQTT.py
+++ testSenderMQTT.py
@@ -1,11 +1,13 @@
 import paho.mqtt.client as mqtt
+import random
+import time
 
 # Definir las funciones de callbacks de conexión y publicación
 def on_connect(client, userdata, flags, rc):
     print("Conectado con resultado de código: "+str(rc))
 
 def on_publish(client, userdata, mid):
-    print("Mensaje publicado en el tópico 'test'")
+    print("Mensaje publicado")
 
 # Crear una instancia de cliente MQTT
 client = mqtt.Client()
@@ -17,9 +19,26 @@ client.on_publish = on_publish
 # Conectarse al broker de Mosquitto en localhost
 client.connect("localhost", 1883, 60)
 
-# Publicar un mensaje de prueba en el tópico 'test'
-message = '[1,2]'
-client.publish("ftf-input", message)
+
+while True:
+    # Generate two random values between 0 and 1 and save them to variables
+    temperatura = random.randint(1,100)
+    humedad = random.randint(1,100)
+    
+
+    messagetemp = str(temperatura)
+    client.publish("modulo1/sensores/temperatura", messagetemp)
+
+    messagehum = str(humedad)
+    client.publish("modulo1/sensores/humedad", messagehum)
+
+    time.sleep(2)
+    
+    # Ask the user if they want to continue the loop
+    #user_input = input("Do you want to continue? (y/n) ")
+    # # If the user enters anything other than 'y', exit the loop
+    # if user_input.lower() != 'y':
+    #     break
 
 # Mantener la conexión MQTT activa
 client.loop_forever()
