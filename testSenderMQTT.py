a962819061c3ba5ac62493f1464e95cb4e92014a
Claudio Vasquez <claudio.vasquez@unah.hn>
2023-05-09 20:57:32 -0600

    edit final

--- testSenderMQTT.py
+++ testSenderMQTT.py
@@ -1,6 +1,8 @@
+import datetime
 import paho.mqtt.client as mqtt
 import random
 import time
+import json
 
 # Definir las funciones de callbacks de conexión y publicación
 def on_connect(client, userdata, flags, rc):
@@ -21,24 +23,67 @@ client.connect("localhost", 1883, 60)
 
 
 while True:
-    # Generate two random values between 0 and 1 and save them to variables
-    temperatura = random.randint(1,100)
-    humedad = random.randint(1,100)
     
-
-    messagetemp = str(temperatura)
-    client.publish("modulo1/sensores/temperatura", messagetemp)
-
-    messagehum = str(humedad)
-    client.publish("modulo1/sensores/humedad", messagehum)
-
-    time.sleep(2)
+    #Agua
+    ph= random.randint(1,100)
+    nivel= random.choice([True, False])
+    tiempoA = datetime.datetime.now()
+    dataA = {
+        "ph": ph,
+        "nivel": nivel,
+        "tiempo": tiempoA.strftime("%Y-%m-%d %H:%M:%S")
+    }
+    messageA = json.dumps(dataA)
+    client.publish("Reserva_Agua", messageA)
     
-    # Ask the user if they want to continue the loop
-    #user_input = input("Do you want to continue? (y/n) ")
-    # # If the user enters anything other than 'y', exit the loop
-    # if user_input.lower() != 'y':
-    #     break
+    #Agua
+    temperatura1 = random.randint(1,100)
+    humedad1 = random.randint(1,100)
+    date1 = datetime.datetime.now()
+    dataM1 = {
+        "temperatura": temperatura1,
+        "humedad": humedad1,
+        "tiempo": date1.strftime("%Y-%m-%d %H:%M:%S")        
+    }
+    message = json.dumps(dataM1)
+    client.publish("modulo1", message)
+    
+    
+    # temperatura2 = random.randint(1,100)
+    # humedad2 = random.randint(1,100)
+    # date2 = datetime.datetime.now()
+    # dataM2 = {
+    #     "temperatura": temperatura2,
+    #     "humedad": humedad2,
+    #     "tiempo": date2.strftime("%Y-%m-%d %H:%M:%S")
+    # }
+    # message2 = json.dumps(dataM2)
+    # client.publish("modulo2", message2)
+    
+    
+    # temperatura3 = random.randint(1,100)
+    # humedad3 = random.randint(1,100)
+    # date3 = datetime.datetime.now()
+    # dataM3 = {
+    #     "temperatura": temperatura3,
+    #     "humedad": humedad3,
+    #     "tiempo": date3.strftime("%Y-%m-%d %H:%M:%S")
+    # }
+    # message3 = json.dumps(dataM3)
+    # client.publish("modulo3", message3)
+    
+    # temperatura4= random.randint(1,100)
+    # humedad4 = random.randint(1,100)
+    # date4= datetime.datetime.now()
+    # dataM4={
+    #     "temperatura": temperatura4,
+    #     "humedad": humedad4,
+    #     "tiempo": date4.strftime("%Y-%m-%d %H:%M:%S")
+    # }
+    # message4 = json.dumps(dataM4)
+    # client.publish("modulo4", message4)
+
+    time.sleep(5)
 
 # Mantener la conexión MQTT activa
 client.loop_forever()
